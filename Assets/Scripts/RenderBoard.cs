71293fe602bcc0961b00ef157a4a410984e922b8
Cyan11 <guilhermelacerda620@gmail.com>
2019-05-04 12:46:03 -0300

    PlayerDataAdded

--- Assets/Scripts/RenderBoard.cs
+++ Assets/Scripts/RenderBoard.cs
@@ -1,8 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-public class RenderBoard : MonoBehaviour
-{
- void Awake() {
-    
- }
+public class RenderBoard : MonoBehaviour {
+	public GameObject testBitcoin;
+
+	Grid grid;
+	int x = -3;
+	int dir = 1;
+	float time;
+	
+	void Awake() {
+		grid = GetComponent<Grid>();
+	}
+
+	void Start() {
+		time = Time.time;
+	}
+
+	void Update() {
+		if (Time.time - time > 0.5f) {
+			x += dir;
+			dir = (x == 3) ? -1 : (x == -3) ? 1 : dir;
+
+			testBitcoin.transform.localPosition =
+				grid.GetCellCenterLocal(new Vector3Int(x, 0, 0));
+			
+			time = Time.time;
+		}
+	}
 }
---
+++ Assets/Scripts/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName="Data/PlayerData")]
+public class PlayerData : ScriptableObject
+{
+   public Board.Cell cellType;
+   public Vector2Int position;
+   public int heldBitcoins, baseBitcoins;
+   public string playerName;
+   //Player functions later
+}
